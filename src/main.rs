d5822f11bcea6690b9ed71b89853ca62cab448f3
Eze <eze.gonzalez@vera.com.uy>
2025-07-15 18:45:25 -0300

    fix en time_diff, que daba vergüenza

--- src/main.rs
+++ src/main.rs
@@ -10,45 +10,31 @@ const ACCEL_POW: f64 = 2.0;
 const MOUSE_SENS: f64 = 0.75;
 const MOUSE_SENS_CAP: f64 = 1.5;
 
-//#[inline(always)]
 fn time_diff(a: &TimeVal, b: &TimeVal) -> f64 {
-    let time = TimeVal {
-        tv_sec: (a.tv_sec - b.tv_sec).abs(),
-        tv_usec: (a.tv_usec - b.tv_usec).abs(),
-    };
-    time.tv_sec as f64 * 1_000f64 + (time.tv_usec as f64 / 1_000f64)
+    let a = a.tv_sec * 1_000_000 + a.tv_usec;
+    let b = b.tv_sec * 1_000_000 + b.tv_usec;
+
+    (a - b).abs() as f64 / 1_000f64
 }
 
-//#[inline(always)]
 fn process_event(event: &mut InputEvent, time_delta: f64) {
     assert!(event.is_type(&EventType::EV_REL));
     if time_delta == 0. {
         println!("To close!");
-        return;
     }
 
     let vel = (event.value as f64 / time_delta).abs();
     let accel_sens = (MOUSE_SENS + (vel * ACCEL_VALUE).powf(ACCEL_POW - 1.)).min(MOUSE_SENS_CAP);
 
     event.value = (event.value as f64 * accel_sens).round() as i32;
-    // println!("{vel}, {accel_sens}");
 }
 
 fn main() {
     // entiendo que por default /dev/input/event5 es el mouse
-    let mut event_num = 5;
-    let mut args = std::env::args();
-    while let Some(arg) = args.next() {
-        match arg.as_str() {
-            "--device" | "-d" => {
-                event_num = args.next().unwrap().parse().unwrap();
-            }
-            _ => {}
-        }
-    }
-
+    let event_num = 5;
     let fd = File::open(format!("/dev/input/event{event_num}")).unwrap();
     let mut mouse = Device::new_from_file(fd).unwrap();
+
     mouse.grab(evdev_rs::GrabMode::Grab).unwrap();
 
     let virt = UInputDevice::create_from_device(&mouse).unwrap();
@@ -64,14 +50,13 @@ fn main() {
     loop {
         // "unwrap" bc there is no way this throws an error in blocking mode
         event = mouse.next_event(ReadFlag::BLOCKING).unwrap().1;
-        // println!("{:?}", event);
         match event.event_code {
-            EventCode::EV_REL(REL_X) | EventCode::EV_REL(REL_Y) => {
+            EventCode::EV_REL(REL_X | REL_Y) => {
                 let delta = time_diff(&event.time, &last_time);
                 process_event(&mut event, delta);
             }
             EventCode::EV_SYN(SYN_REPORT) => {
-                last_time = event.time.clone();
+                last_time = event.time;
             }
             EventCode::EV_SYN(SYN_DROPPED) => {
                 panic!(" --- DROPPED DROPPED DROPPED --- ")
